d3ea264ddf17a15468fd14d20a6a1cfbba4d6e2e
Allisson Santos <allisson.ferreira@outlook.com>
2021-03-18 21:01:22 -0300

    Create SideBar interface

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,14 @@
-export function SideBar() {
+interface SideBarProps {
+  genres: Array<{
+    id: number;
+    title: string;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  }>;
+  iconName: string;
+  selectedGenreId: number;
+  handleClickButton: Function;
+}
+
+export function SideBar(props: SideBarProps) {
   // Complete aqui
 }
