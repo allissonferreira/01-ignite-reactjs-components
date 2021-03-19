e725a4dd1f04955d5e2e5adb5b5eba5eb3855108
Allisson Santos <allisson.ferreira@outlook.com>
2021-03-18 21:02:32 -0300

    Add nav element HTML & Import Button component

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,5 @@
+import { Button } from './Button';
+
 interface SideBarProps {
   genres: Array<{
     id: number;
@@ -14,4 +16,24 @@ export function SideBar(props: SideBarProps) {
   const genres = props.genres;
   const selectedGenreId = props.selectedGenreId;
   const handleClickButton = props.handleClickButton;
+  
+  return (
+    <>
+      <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+      </nav>
+    </>
+  );
 }
